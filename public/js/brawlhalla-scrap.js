1a80b26246fd3b865c082dc0bc1018c81e3c0918
Douwdy <mrslim65.gl@gmail.com>
2022-09-15 18:31:22 +0200

    Add Levels + Js logic

--- public/js/brawlhalla-scrap.js
+++ public/js/brawlhalla-scrap.js
@@ -1,14 +1,44 @@
 // DATA SOURCES
 let playerID = 5466734;
-import playerFile from './player.json' assert {type: 'json'};
-const apiUrl = `https://brawlhallastats.herokuapp.com/api/submit-form3-by-id?player=${playerID}`
-// GET json data from apiUrl and return a JSON object
-fetch(apiUrl,
-    function (error, response) {
-        if (error) {
-            console.log(error);
-            }
-            else {
-                console.log(response);
-                }
-            });
+const apiUrl = `https://brawlhallastats.herokuapp.com/api/submit-form3-by-id?player=${playerID}`;
+const playerData = "./public/js/player.json";
+// const legendLocation = document.getElementById(`${data.legends.legend_name_key}`);
+let legendsData = [];
+// fetching the playerdata from player.json
+fetch(playerData).then(response => {
+    return response.json();
+}).then(data => {
+    let legendsData = data.legends;
+    for (let i = 0; i < legendsData.length; i++) {
+        let legends = legendsData[i];
+        let legendNameData = legends.legend_name_key;
+        let legendName = legendNameData.replace(/\s+/g, '');
+        let legendLevel = legends.level;
+        let legendLocation = document.getElementById(`${legendName}`);
+        console.log(legendName + ": " + legendLevel);
+        if (legendLevel >= 25) {
+            legendLocation.innerHTML = `
+                <img src="./public/img/legends/Portrait_${legendName}.png" alt="${legendName} frame">
+                <div class="container-character__checkmark">
+                    <i class="fa-solid fa-circle-check"></i>
+                </div>
+                <div class="container-character__level">
+                    <h2 style="color: rgb(125, 255, 125)">${legendLevel}</h2>
+                </div>
+                `;
+            } else {
+                legendLocation.innerHTML = `
+                    <img src="./public/img/legends/Portrait_${legendName}.png" alt="${legendName} frame">
+                    <div class="container-character__checkmark-none">
+                        <i class="fa-solid fa-circle-xmark"></i>
+                    </div>
+                    <div class="container-character__level">
+                        <h2 style="color: crimson">${legendLevel}</h2>
+                    </div>
+                    `;
+        }
+    }
+}
+).catch(error => {
+    console.log(error);
+});
