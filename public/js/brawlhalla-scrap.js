aa91e8218a949de01958bc6821f7ae1f3836604c
Douwdy <mrslim65.gl@gmail.com>
2022-09-15 16:27:34 +0200

    JS Test

--- public/js/brawlhalla-scrap.js
+++ public/js/brawlhalla-scrap.js
@@ -1,39 +1,14 @@
-const fetch = require("node-fetch");
-const cheerio = require("cheerio");
-const fs = require("fs");
-// function to get the raw data
-const getRawData = (URL) => {
-   return fetch(URL)
-      .then((response) => response.text())
-      .then((data) => {
-         return data;
-      });
-};
-// URL for data
-const URL = "https://www.stats.brawlhalla.fr/player/5466734";
-// start of the program
-const getBrawlhallaCharacterLevel = async () => {
-   const rawData = await getRawData(URL);
-   // parsing the data
-   const parsedData = cheerio.load(rawData);
-   console.log(parsedData);
-   // write code to extract the data
-   // extracting character name
-   const nameColumn = columns[0];
-   if (nameColumn) {
-      character = nameColumn.children[0];
-      if (character) {
-        character = character.children[0].data;
-      }
-   }
-   // extracting the level of the character
-   const levelColumn = columns[3];
-   if (levelColumn) {
-      level = levelColumn.children[1];
-      if (level) {
-        level = level.children[0].data;
-      }
-   }
-};
-// invoking the main function
-getBrawlhallaCharacterLevel();
+// DATA SOURCES
+let playerID = 5466734;
+import playerFile from './player.json' assert {type: 'json'};
+const apiUrl = `https://brawlhallastats.herokuapp.com/api/submit-form3-by-id?player=${playerID}`
+// GET json data from apiUrl and return a JSON object
+fetch(apiUrl,
+    function (error, response) {
+        if (error) {
+            console.log(error);
+            }
+            else {
+                console.log(response);
+                }
+            });
