b778eaa07e0d4ed09a36144e3b9ae5c9dfb40eb1
Guillaume Lamarque <guillaumelamarque7@gmail.com>
2023-08-22 12:26:31 +0200

    Api Update ⚙️

--- public/js/brawlhalla-processing.js
+++ public/js/brawlhalla-processing.js
@@ -1,5 +1,5 @@
 // Brawlhalla Patch version
-let patchID = 49;
+let patchID = 51;
 
 // Get brawlhalla id from input field brawlhallaID and update the playerID variable for the json call
 function getBrawlhallaID() {
